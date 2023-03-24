3136245402052e5fdfcfc5c15e29b9ad3d22ef1c
jinhyun <0311wlsgus@gmail.com>
2023-03-24 11:13:53 +0900

    23d

--- Java/src/chap4/Student.java
+++ Java/src/chap4/Student.java
@@ -36,21 +36,39 @@ public class Student {
 		예외처리를 해서, 다시 점수를 입력받을 수 있는
 		무한 루프를 구성해 주세요.
 		*/
+//		while(true) {
+//			System.out.println("- 국어: ");
+//			this.kor = sc.nextInt();
+//			System.out.println("- 영어: ");
+//			this.eng = sc.nextInt();
+//			System.out.println("- 수학: ");
+//			this.math = sc.nextInt();
+//			if(kor<0 || kor>100) {
+//				System.out.println("옳바른 값을 입력해주세요.");
+//			} else if(eng<0 || eng>100) {
+//				System.out.println("옳바른 값을 입력해주세요.");
+//			} else if(math<0 || math>100) {
+//				System.out.println("옳바른 값을 입력해주세요.");
+//			}
+//			break;
+		
 		while(true) {
-			System.out.println("- 국어: ");
-			this.kor = sc.nextInt();
-			System.out.println("- 영어: ");
-			this.eng = sc.nextInt();
-			System.out.println("- 수학: ");
-			this.math = sc.nextInt();
-			if(kor<0 || kor>100) {
-				System.out.println("옳바른 값을 입력해주세요.");
-			} else if(eng<0 || eng>100) {
-				System.out.println("옳바른 값을 입력해주세요.");
-			} else if(math<0 || math>100) {
-				System.out.println("옳바른 값을 입력해주세요.");
+			try {
+				System.out.println("국어: ");
+				this.kor = sc.nextInt();
+				System.out.println("영어: ");
+				this.eng = sc.nextInt();
+				System.out.println("수학: ");
+				this.math = sc.nextInt();
+				break;
+			} catch (Exception e) {
+				System.out.println("점수는 숫자로만 입력하세요.");
+				sc.nextLine(); // 잘못된 값을 가져가기 위해, 안하면 무한루프에 빠진다.
 			}
-			break;
+		
+		
+		
+		
 		}
 		
 	}
@@ -67,7 +85,7 @@ public class Student {
 	}
 
 	//학생의 성적 정보를 출력하는 메서드
-	public void outputStuInfo() {
+	public void outputStuInfo() { // %숫자d 이런식으로 나오면 숫자만큼 공간을 만들어준다, 반듯하게 하고 싶을떄 사용, 음수로 하면 앞에 공간지정 
 		System.out.printf("%4s %6s %6d점 %5d점 %7d점 %7d점 %7.2f점 %6s\n"
 				, this.stuId, this.name, this.kor, this.eng, 
 				this.math, this.total, this.average, this.grade);
